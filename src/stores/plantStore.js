c1c635d09da64aee1c097443dd9bbcaf7ec4aff9
EmanAALK <emanalkandarii@gmail.com>
2020-08-10 18:06:32 +0300

    Something is never taken away from you without being REPLACED with something greater. You may have to wait for it however. It's test of faith. axios has been replaced with instance.

--- src/stores/plantStore.js
+++ src/stores/plantStore.js
@@ -1,5 +1,7 @@
-import { decorate, observable, isObservableArray } from "mobx";
-import axios from "axios";
+import { decorate, observable } from "mobx";
+
+//Stores
+import instance from "./instance";
 
 class PlantStore {
   plants = [];
@@ -7,7 +9,7 @@ class PlantStore {
 
   fetchPlants = async () => {
     try {
-      const res = await axios.get("http://localhost:8000/plants");
+      const res = await instance.get("/plants");
       this.plants = res.data;
       this.loading = false;
     } catch (error) {
@@ -21,10 +23,7 @@ class PlantStore {
     try {
       const formData = new FormData();
       for (const key in newPlant) formData.append(key, newPlant[key]);
-      const res = await axios.post(
-        `http://localhost:8000/vendors/${vendorId}/plants`,
-        formData
-      );
+      const res = await instance.post(`/vendors/${vendorId}/plants`, formData);
       this.plants.push(res.data);
       vendor.plants.push({ id: res.data.id });
     } catch (error) {
@@ -34,7 +33,7 @@ class PlantStore {
 
   deletePlant = async (plantId) => {
     try {
-      await axios.delete(`http://localhost:8000/plants/${plantId}`);
+      await instance.delete(`/plants/${plantId}`);
       this.plants = this.plants.filter((plant) => plant.id !== plantId);
     } catch (error) {
       console.error("PlantStore -> deletePlant -> error", error);
@@ -45,10 +44,7 @@ class PlantStore {
     try {
       const formData = new FormData();
       for (const key in newPlant) formData.append(key, newPlant[key]);
-      await axios.put(
-        `http://localhost:8000/plants/${updatedPlant.id}`,
-        updatedPlant
-      );
+      await instance.put(`/plants/${updatedPlant.id}`, updatedPlant);
       const plant = this.plants.find((plant) => plant.id === updatedPlant.id);
       for (const key in plant) plant[key] = updatedPlant[key];
     } catch (error) {
