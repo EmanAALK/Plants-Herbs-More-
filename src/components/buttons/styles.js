2ddad2888a15d7920a4294fb11e2a6d16c78715d
EmanAALK <emanalkandarii@gmail.com>
2020-08-10 17:03:54 +0300

    Auth styling

--- src/components/buttons/styles.js
+++ src/components/buttons/styles.js
@@ -1,5 +1,14 @@
 import styled from "styled-components";
 
+export const AuthButtonStyled = styled.button`
+  font-size: 1em;
+  padding: 0.25em 1em;
+  margin-left: 0.5em;
+  border-radius: 3px;
+  background-color: ${(props) => props.theme.gold};
+  color: ${(props) => props.theme.gold};
+`;
+
 export const DeleteButtonStyled = styled.p`
   color: ${(props) => props.theme.red};
 `;
